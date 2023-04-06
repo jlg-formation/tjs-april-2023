fb0b210f406560febe1597be0ee8aa848eea9df7
Jean-Louis GUENEGO <jlguenego@gmail.com>
2023-04-06 11:41:21 +0200

    dev: added dom liste route

--- front/src/stock/routes/ListRoute.tsx
+++ front/src/stock/routes/ListRoute.tsx
@@ -1,7 +1,50 @@
 import { css } from '@emotion/react'
+import { Link } from 'react-router-dom'
 
 const ListRoute = () => {
-  return <main css={style}>List Route</main>
+  return (
+    <main css={style}>
+      <h1>Liste des articles</h1>
+      <div className="content">
+        <nav>
+          <button>Rafraîchir</button>
+          <Link to="add">Ajouter</Link>
+          <button>Supprimer</button>
+        </nav>
+        <table>
+          <thead>
+            <tr>
+              <th className="name">Nom</th>
+              <th className="price">Prix</th>
+              <th className="qty">Quantité</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td className="name">Tournevis</td>
+              <td className="price">2.30 €</td>
+              <td className="qty">110</td>
+            </tr>
+            <tr>
+              <td className="name">Marteau</td>
+              <td className="price">5.00 €</td>
+              <td className="qty">23</td>
+            </tr>
+            <tr>
+              <td className="name">Serpe</td>
+              <td className="price">8.00 €</td>
+              <td className="qty">8</td>
+            </tr>
+            <tr>
+              <td className="name">Perceuse</td>
+              <td className="price">45.50 €</td>
+              <td className="qty">10</td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
+    </main>
+  )
 }
 
 export default ListRoute
