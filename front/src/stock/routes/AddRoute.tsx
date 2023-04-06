aee6470eb832049305098b0deedb076b49dc9b78
Jean-Louis GUENEGO <jlguenego@gmail.com>
2023-04-06 13:43:21 +0200

    dev: added css for add route

--- front/src/stock/routes/AddRoute.tsx
+++ front/src/stock/routes/AddRoute.tsx
@@ -1,7 +1,26 @@
 import { css } from '@emotion/react'
 
 const AddRoute = () => {
-  return <main css={style}>Add Route</main>
+  return (
+    <main css={style}>
+      <h1>Ajout d'un article</h1>
+      <form>
+        <label>
+          <span>Nom</span>
+          <input type="text" />
+        </label>
+        <label>
+          <span>Prix</span>
+          <input type="number" />
+        </label>
+        <label>
+          <span>Quantité</span>
+          <input type="number" />
+        </label>
+        <button className="primary">Ajouter</button>
+      </form>
+    </main>
+  )
 }
 
 export default AddRoute
