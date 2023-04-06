de09b2455f4c04e425025ddf98ca5c165de75e53
Jean-Louis GUENEGO <jlguenego@gmail.com>
2023-04-06 09:47:32 +0200

    dev: route

--- front/src/routes/LegalRoute.tsx
+++ front/src/routes/LegalRoute.tsx
@@ -1,5 +1,89 @@
 const LegalRoute = () => {
-  return <p>Legal works!</p>
+  return (
+    <main>
+      <h1>Mentions Légales</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatem ipsa
+        itaque maiores dolorum consequatur dolor excepturi odio porro iusto
+        officiis iste dolorem expedita, perspiciatis, inventore magnam
+        voluptatibus consequuntur maxime neque!
+      </p>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequuntur
+        quidem sint nostrum dolore rerum soluta doloremque! Deleniti, ut
+        quisquam molestiae libero modi vero! Voluptas voluptatem modi dolore in
+        quae id mollitia obcaecati ipsum consectetur dolorum accusantium ut
+        deleniti nostrum, cumque provident repellendus nesciunt natus culpa
+        pariatur temporibus voluptate molestias exercitationem maiores animi.
+        Autem, vel sint ut aperiam, quis maiores suscipit soluta tenetur impedit
+        laudantium alias ab aut dolore nobis harum asperiores excepturi,
+        eligendi nisi a in. Excepturi culpa officiis facere consequuntur
+        dignissimos, alias nemo quas voluptatibus amet deserunt voluptate eaque
+        ut eos tempora in fugit quisquam, porro debitis quod suscipit!
+      </p>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatem ipsa
+        itaque maiores dolorum consequatur dolor excepturi odio porro iusto
+        officiis iste dolorem expedita, perspiciatis, inventore magnam
+        voluptatibus consequuntur maxime neque!
+      </p>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequuntur
+        quidem sint nostrum dolore rerum soluta doloremque! Deleniti, ut
+        quisquam molestiae libero modi vero! Voluptas voluptatem modi dolore in
+        quae id mollitia obcaecati ipsum consectetur dolorum accusantium ut
+        deleniti nostrum, cumque provident repellendus nesciunt natus culpa
+        pariatur temporibus voluptate molestias exercitationem maiores animi.
+        Autem, vel sint ut aperiam, quis maiores suscipit soluta tenetur impedit
+        laudantium alias ab aut dolore nobis harum asperiores excepturi,
+        eligendi nisi a in. Excepturi culpa officiis facere consequuntur
+        dignissimos, alias nemo quas voluptatibus amet deserunt voluptate eaque
+        ut eos tempora in fugit quisquam, porro debitis quod suscipit!
+      </p>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatem ipsa
+        itaque maiores dolorum consequatur dolor excepturi odio porro iusto
+        officiis iste dolorem expedita, perspiciatis, inventore magnam
+        voluptatibus consequuntur maxime neque!
+      </p>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequuntur
+        quidem sint nostrum dolore rerum soluta doloremque! Deleniti, ut
+        quisquam molestiae libero modi vero! Voluptas voluptatem modi dolore in
+        quae id mollitia obcaecati ipsum consectetur dolorum accusantium ut
+        deleniti nostrum, cumque provident repellendus nesciunt natus culpa
+        pariatur temporibus voluptate molestias exercitationem maiores animi.
+        Autem, vel sint ut aperiam, quis maiores suscipit soluta tenetur impedit
+        laudantium alias ab aut dolore nobis harum asperiores excepturi,
+        eligendi nisi a in. Excepturi culpa officiis facere consequuntur
+        dignissimos, alias nemo quas voluptatibus amet deserunt voluptate eaque
+        ut eos tempora in fugit quisquam, porro debitis quod suscipit!
+      </p>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatem ipsa
+        itaque maiores dolorum consequatur dolor excepturi odio porro iusto
+        officiis iste dolorem expedita, perspiciatis, inventore magnam
+        voluptatibus consequuntur maxime neque!
+      </p>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequuntur
+        quidem sint nostrum dolore rerum soluta doloremque! Deleniti, ut
+        quisquam molestiae libero modi vero! Voluptas voluptatem modi dolore in
+        quae id mollitia obcaecati ipsum consectetur dolorum accusantium ut
+        deleniti nostrum, cumque provident repellendus nesciunt natus culpa
+        pariatur temporibus voluptate molestias exercitationem maiores animi.
+        Autem, vel sint ut aperiam, quis maiores suscipit soluta tenetur impedit
+        laudantium alias ab aut dolore nobis harum asperiores excepturi,
+        eligendi nisi a in. Excepturi culpa officiis facere consequuntur
+        dignissimos, alias nemo quas voluptatibus amet deserunt voluptate eaque
+        ut eos tempora in fugit quisquam, porro debitis quod suscipit!
+      </p>
+    </main>
+  )
 }
 
 export default LegalRoute
