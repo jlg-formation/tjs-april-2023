cef84d85dacf7af28cc6ce72f340202864fe2030
Jean-Louis GUENEGO <jlguenego@gmail.com>
2023-04-07 14:39:01 +0200

    dev: added articles retrieve all

---
+++ back/src/api.ts
@@ -0,0 +1,25 @@
+import express from "express";
+import { Article } from "./interfaces/Article";
+
+const articles: Article[] = [
+  {
+    id: "a1",
+    name: "Tournevis",
+    price: 12.23,
+    qty: 120,
+  },
+  {
+    id: "a2",
+    name: "Pelle",
+    price: 50,
+    qty: 6,
+  },
+];
+
+const app = express.Router();
+
+app.get("/articles", (req, res) => {
+  res.json(articles);
+});
+
+export const api = app;
--- back/src/server.ts
+++ back/src/server.ts
@@ -1,6 +1,8 @@
 import express from "express";
 import serveIndex from "serve-index";
 
+import { api } from "./api";
+
 const app = express();
 const port = 3000;
 const publicDir = ".";
@@ -9,6 +11,8 @@ app.use((req, res, next) => {
   console.log("req: ", req.method, req.path);
   next();
 });
+
+app.use("/api", api);
 
 app.use(express.static(publicDir));
 app.use(serveIndex(publicDir, { icons: true }));
